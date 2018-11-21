d0ee52d0dab2ed8821a148b8d3d13357a256f7d0
bluefriday <trylhc@naver.com>
2018-11-21 12:13:16 +0900

    Update Card.h
    list.remove를 위하여 equal 연산자 overriding

--- Blackjack/Card.h
+++ Blackjack/Card.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #ifndef CARD_H
@@ -9,7 +8,20 @@ public:
 	int number;
 	bool isNotNumber;
 	std::string type;
-	Card(int);
+	Card(int );
+
+	//remove는 ==, != 로 매칭을 하는데, custom object 에는 그게 없음
+	//overriding 해서 만들어줘야함.
+	bool operator == (const Card& card) const 
+	{ 
+		return number == card.number && isNotNumber == card.isNotNumber && type == card.type;
+	}
+	bool operator != (const Card& card) const
+	{
+		return !(operator==(card));
+	}
+
 };
 
 #endif
+
