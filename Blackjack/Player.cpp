f893b5eedb576f1e747090cb2dd372034e0f6d99
bluefriday <trylhc@naver.com>
2018-11-21 12:15:32 +0900

    오브젝트 주소 전달로 변경

--- Blackjack/Player.cpp
+++ Blackjack/Player.cpp
@@ -1,15 +1,14 @@
-
 #include "pch.h"
 #include "Player.h"
 
-Player::Player(Deck deck)
+Player::Player(Deck* deck)
 {
 	this->playerStatus = "hit";
-	myHand.push_back(&deck.pickOneCard);
-	//myHand.push_back(&deck.pickOneCard);
+	myHand.push_back(deck->pickOneCard());
+	myHand.push_back(deck->pickOneCard());
 }
 
-void Player::hit(Deck deck) 
+void Player::hit(Deck* deck)
 {
 	this->playerStatus = "hit";
 }
@@ -25,19 +24,38 @@ void Player::showMyCard()
 	std::string finalNum = "";
 	std::list<Card>::iterator itr;
 
-	std::cout << this->playerType + "ÀÇ Ä«µå :" << std::endl;
+	std::cout << this->playerType + "ì˜ ì¹´ë“œëŠ” :";
 
-	for (itr=myHand.begin() ; itr != myHand.end() ; itr++)
+	for (itr = myHand.begin(); itr != myHand.end(); itr++)
 	{
 		Card card = *itr;
 		num = card.number;
 
-		if (num == 11)		{	finalNum = "[J]";		}
-		else if (num == 12) {	finalNum = "[Q]";		}
-		else if (num == 13) {	finalNum = "[K]";		}
+		if (num == 11) { finalNum = "[J]"; }
+		else if (num == 12) { finalNum = "[Q]"; }
+		else if (num == 13) { finalNum = "[K]"; }
 		else { finalNum = "[" + std::to_string(num) + "]"; }
 
 		std::cout << finalNum + " ";
 	}
 	std::cout << std::endl;
+}
+
+int Player::countMyScore() {
+	int score = 0;
+	int num = 0;
+	std::list<Card>::iterator itr;
+
+	for (itr = myHand.begin(); itr != myHand.end(); itr++)
+	{
+		Card card = *itr;
+		num = card.number;
+
+		if (num > 10) {
+			num = 10;
+		}
+
+		score += num;
+	}
+	return score;
 }
